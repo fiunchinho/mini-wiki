7afef13e180271d373c5707ae3b39d367ec9b873
Jose Armesto <github@armesto.net>
2015-03-14 16:47:01 +0000

    Since we use HTTP Cache, it's better to throw exception and rely on 'stale-if-error' if lock can't get acquired when reading file

--- src/MiniWiki/Wiki.php
+++ src/MiniWiki/Wiki.php
@@ -29,16 +29,31 @@ class Wiki
         }
     }
 
+    /**
+     * A function like file_get_contents that makes use of locks.
+     *
+     * I don't want PHP to read a file while is being written by other process, so depend on
+     * pessimistic locks. Instead of waiting for the lock to be released (like in writeArticle()),
+     * it throws an exception if it can't get the lock. This prevents many concurrent users waiting
+     * to get the lock, instead of getting cached content.
+     *
+     * Since we rely on HTTP Cache using a Gateway cache or Reverse Proxy cache, with stale-if-error
+     * enabled, requests that can't get the file lock will see an stale cached version of the page,
+     * without waiting for the lock.
+     *
+     * @param  string $filename
+     * @return string               File content
+     * @throws \RunTimeException    If can't get file lock
+     */
     private function file_get_contents_lock($filename){
         $handle = fopen($filename, 'r');
-        if (flock($handle, LOCK_EX)){
+        if (flock($handle, LOCK_EX | LOCK_NB)){
             $content = file_get_contents($filename);
             flock($handle, LOCK_UN);
             fclose($handle);
 
             return $content;
         }
-
         throw new \RunTimeException("Couldn't get file lock");
     }
 }
