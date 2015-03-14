e37c01b1145f0742476003cd1a9348649b9235e2
Jose Armesto <github@armesto.net>
2015-03-14 17:12:45 +0000

    Changed cache folder

--- resources/config/dev.php
+++ resources/config/dev.php
@@ -4,6 +4,6 @@ $app['debug']                       = true;
 $app['twig.path']                   = [__DIR__.'/../templates'];
 $app['twig.options']                = ['cache' => '/tmp/cache/twig'];
 $app['article_path']                = __DIR__ . '/../db.db';
-$app['http_cache.cache_dir']        = '/tmp/cache/';
+$app['http_cache.cache_dir']        = '/tmp/cache/http';
 $app['http_cache.esi']              = null;
 $app['http_cache.max_age_seconds']  = 10;
